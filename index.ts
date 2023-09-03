bead3de1d86753891b0229d0b155d0ac0aebac4f
Hiroki <s1f102102548@iniad.org>
2023-09-03 17:22:07 +0900

    両方動く

--- index.ts
+++ index.ts
@@ -3,37 +3,75 @@ import {SerialPort} from 'serialport';
 const path = "COM3"
 const port = new SerialPort({path, baudRate: 9600});
 
-const speed = 1;
+const speed = 10;
 const LEFT = 0;
 const RIGHT = 1;
 const SEITEN = 0;
 const HANTEN = 1;
 
 
-const moterType = LEFT;
-const direction = HANTEN
+let moterType = RIGHT;
+let direction = SEITEN;
 
-const data = Buffer.from([speed << 2 | moterType << 1 | direction])
+// setInterval(()=>{
+//     moterType = 1 - moterType
+//     console.log(moterType)
+// },100);
+
+
+
+const data = Buffer.from([speed << 2 | 0 << 1 | direction])
+const data1 = Buffer.from([speed << 2| 1 << 1 | direction])
 // const data = (speed << 2) | (moterType << 1) | direction
 // const data = `${String.fromCharCode(speed)}${String.fromCharCode(moterType)}${String.fromCharCode(direction)}`;
 // const data1 = data.toString()
 // const data = Buffer.from([speed, moterType, direction]);
 
+let flag = true
 
+
+const run=()=>{
+    port.write(data, function(err){
+        if(err){
+            return console.log('送信失敗',err.message);
+        }
+        console.log("送信成功");
+    })
+}
+
+const run1 =()=>{
+    port.write(data1, function(err){
+        if(err){
+            return console.log('送信失敗',err.message);
+        }
+        console.log("送信成功");
+    })
+}
 console.log("data",data)
 
 port.on('open', function() {
     console.log('ポートが開いた');
+
     
+    // setInterval(()=>{
+    
+    
+    //     port.write(data, function(err){
+    //         if(err){
+    //             return console.log('送信失敗',err.message);
+    //         }
+    //         console.log("送信成功");
+    //     })
+    
+    
+    // },1000)
+
     setInterval(()=>{
-        port.write(data, function(err){
-            if(err){
-                return console.log('送信失敗',err.message);
-            }
-            console.log("送信成功");
-        })
+        run()
+        run1()
     },1000)
     
+   
     let text = ''
 
     // port.on('data', function(data) {
