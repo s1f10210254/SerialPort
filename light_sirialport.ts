7ac007a2e50876bf3c68d194d989a1ca8d86cfa8
Hiroki <s1f102102548@iniad.org>
2023-09-09 15:35:17 +0900

    a

---
+++ light_sirialport.ts
@@ -0,0 +1,25 @@
+import { SerialPort } from "serialport";
+
+const path = 'COM4'
+const port = new SerialPort({path, baudRate:9600});
+
+const run = () =>{
+
+    const data = Buffer.from([11111111])
+    port.write(data,function(err){
+        if(err){
+            return console.log('送信失敗', err.message);
+        }
+        console.log("run送信成功");
+    })
+}
+
+port.on('open', function(){
+    console.log('ポートが開いた');
+
+    let n = 0;
+
+    setInterval(() =>{
+        run();
+    })
+})
