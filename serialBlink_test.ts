df63774015394ea67eb2c67310ae41a90e89e3df
Hiroki <s1f102102548@iniad.org>
2023-09-11 20:07:07 +0900

    a

--- serialBlink_test.ts
+++ serialBlink_test.ts
@@ -5,23 +5,22 @@ const path = "COM4"
 
 const port = new SerialPort({path, baudRate:57600});
 
-function sendDigitalMessage(pin: number, value:number){
-    const command = 0x9D;
-
-    const data = [command, value];
-
-    port.write(data,(err)=>{
-        if(err){
-            return console.log('Error:', err.message);
-        }
-        console.log(`Sent to pin ${pin}: ${data.map(byte => byte.toString(16)).join(' ')}`)
-    })
-}
-
-port.on('open', ()=>{
+port.on('open', () => {
     console.log('Arduino connected');
-
-    for (let value = 0; value <= 1; value++){
-        sendDigitalMessage(13,value)
+  
+    // 上位1バイト（種別コード）の全通りを送信
+    for (let command = 0x90; command <= 0x9F; command++) {
+      sendDigitalMessage(13, command, 1); // 下位1バイトを1（ON）で固定
+      sendDigitalMessage(13, command, 0); // 下位1バイトを0（OFF）で固定
     }
-})
+  });
+  
+  function sendDigitalMessage(pin: number, command: number, value: number) {
+    const data = [command, value];
+    port.write(data, (err) => {
+      if (err) {
+        return console.log('Error:', err.message);
+      }
+      console.log(`Sent to pin ${pin}, command ${command.toString(16)}: ${data.map(byte => byte.toString(16)).join(' ')}`);
+    });
+  }
