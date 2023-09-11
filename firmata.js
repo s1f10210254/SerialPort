b0a1031fdaa7c79365e3170cdbd1f66789ff861d
Hiroki <s1f102102548@iniad.org>
2023-09-12 01:04:30 +0900

    a

---
+++ firmata.js
@@ -0,0 +1,4 @@
+// const Firmata = require("firmata");
+// import {board, Led} from "firmata"
+
+// const board = new Firmata("")
--- serialBlink_test.ts
+++ serialBlink_test.ts
@@ -1,4 +1,5 @@
 import { SerialPort, ReadlineParser } from "serialport";
+import { buffer } from "stream/consumers";
 
 
 
@@ -8,28 +9,33 @@ const port = new SerialPort({path, baudRate:57600});
 
 const parser = port.pipe(new ReadlineParser({ delimiter: '\n'}));
 
+const identifierCode = 0x9D;
+const upperByteValues = [...Array(128).keys()]; // 0から127の上位バイトの値
+const lowerByteValues = [0x00, 0x01]; // 下位バイトの値
+
+console.log(Buffer.from([identifierCode]))
+
 port.on('open', () => {
     console.log('Arduino connected');
-    sendAllCombinations();
-});
-
-
-function sendDigitalMessage(pin:number, value:number){
-    const command = 0x9D;
-    const portNumber = pin <= 7 ? 0x90 : 0x98;
-    const data = [command | (pin % 8), value];
-    const message = [command, ...data]
-
-    port.write(message);
-    console.log(`Sent: 0x${command.toString(16)} 0x${data[0].toString(16)} 0x${data[1].toString(16)}`);
-}
-   
   
-function sendAllCombinations() {
-    for (let upperByte = 0; upperByte <= 0x7F; upperByte++) {
-      for (let lowerByte = 0; lowerByte <= 1; lowerByte++) {
-        const value = (upperByte << 1) | lowerByte;
-        sendDigitalMessage(13, value);
-      }
-    }
-  }
+    upperByteValues.forEach((upperByte) => {
+      lowerByteValues.forEach((lowerByte) => {
+        // 上位バイトと下位バイトを送信
+        const buffer = Buffer.from([identifierCode, upperByte, lowerByte]);
+        port.write(buffer, (err) => {
+          if (err) {
+            return console.error('Error writing to port: ', err.message);
+          }
+          console.log(`Sent: 0x${identifierCode.toString(16)} 0x${upperByte.toString(16)} 0x${lowerByte.toString(16)}`);
+        });
+      });
+    });
+  });
+  
+  parser.on('data', (data) => {
+    console.log(`Received: ${data}`);
+  });
+  
+  port.on('error', (err) => {
+    console.error('Error: ', err);
+  });
