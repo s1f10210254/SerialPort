9f835ce2136706c983d0bb7295216660a1ea1290
Hiroki <s1f102102548@iniad.org>
2023-09-12 01:38:29 +0900

    全部送ってるが信号が違うらしい

--- serialBlink_test.ts
+++ serialBlink_test.ts
@@ -1,3 +1,4 @@
+import { error } from "console";
 import { SerialPort, ReadlineParser } from "serialport";
 import { buffer } from "stream/consumers";
 
@@ -40,34 +41,83 @@ console.log(Buffer.from([identifierCode]))
 //     console.error('Error: ', err);
 //   });
 
-port.on('open', ()=>{
-    console.log("Arduino connected");
 
-    upperByteValues.forEach((upperByte) =>{
-        lowerByteValues.forEach((lowerByte)=>{
-            //最初に識別コードを送信
-            port.write(Buffer.from([identifierCode]), (err) =>{
+
+//順番になってない
+// port.on('open', ()=>{
+//     console.log("Arduino connected");
+
+//     upperByteValues.forEach((upperByte) =>{
+//         lowerByteValues.forEach((lowerByte)=>{
+//             //最初に識別コードを送信
+//             port.write(Buffer.from([identifierCode]), (err) =>{
+//                 if(err){
+//                     return console.log('Error writeing to port: ', err.message);
+//                 }
+//                 console.log(`Sent identifireCode: 0x${identifierCode.toString(16)}`)
+//                 // 次に上位バイトを送信
+//                 port.write(Buffer.from([upperByte]),(err)=>{
+//                     if(err){
+//                         return console.error('Error writeing to port: ',err.message);
+//                     }
+//                     console.log(`Sent upperByte: 0x${upperByte.toString(16)}`)
+//                     //最後に下位バイトを送信
+//                     port.write(Buffer.from([lowerByte]), (err)=>{
+//                         if(err){
+//                             return console.error('Error writing to port: ', err.message);
+//                         }
+//                         console.log(`Sent lowerByte: 0x${lowerByte.toString(16)}`)
+//                     })
+//                 })
+//             })
+//         })
+//     })
+// })
+
+port.on('open', ()=>{
+    console.log('Arduino connected');
+
+    let setIndex = 0;
+
+    //セットごとにデータを送信
+    const sendNextSet = ()=>{
+        if(setIndex < upperByteValues.length * lowerByteValues.length){
+            const upperByteIndex = Math.floor(setIndex / lowerByteValues.length);
+            const lowerByteIndex = setIndex % lowerByteValues.length;
+            const upperByte = upperByteValues[upperByteIndex];
+            const lowerByte = lowerByteValues[lowerByteIndex];
+
+            //セットの識別コード(0x9D)を最初に送信
+            port.write(Buffer.from([identifierCode]), (err)=>{
                 if(err){
-                    return console.log('Error writeing to port: ', err.message);
+                    return console.error('Error writing to port: ', err.message);
                 }
-                console.log(`Sent identifireCode: 0x${identifierCode.toString(16)}`)
-                // 次に上位バイトを送信
-                port.write(Buffer.from([upperByte]),(err)=>{
-                    if(err){
-                        return console.error('Error writeing to port: ',err.message);
+                console.log(`Sent identifierCode: 0x${identifierCode.toString(16)}`);
+
+                // セット内の上位バイトを送信
+                port.write(Buffer.from([upperByte]), (err) => {
+                    if (err) {
+                        return console.error('Error writing to port: ', err.message);
                     }
-                    console.log(`Sent upperByte: 0x${upperByte.toString(16)}`)
-                    //最後に下位バイトを送信
-                    port.write(Buffer.from([lowerByte]), (err)=>{
-                        if(err){
+                    console.log(`Sent upperByte: 0x${upperByte.toString(16)}`);
+
+                    // セット内の下位バイトを送信
+                    port.write(Buffer.from([lowerByte]), (err) => {
+                        if (err) {
                             return console.error('Error writing to port: ', err.message);
                         }
-                        console.log(`Sent lowerByte: 0x${lowerByte.toString(16)}`)
-                    })
-                })
+                        console.log(`Sent lowerByte: 0x${lowerByte.toString(16)}`);
+
+                        //次のセットへ進む
+                        setIndex ++;
+                        sendNextSet();
+                    });
+                });
+            
             })
-        })
-    })
+        }
+    };
+    sendNextSet()
 })
 parser.on('data', (data) => {
     console.log(`Received: ${data}`);
